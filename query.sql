d45bfd39b2de335708b2789603588fcd037cc2b6
Marc Dix <marcdix@marcdix.de>
2023-03-16 15:55:50 +0100

    ignore contract that abuses meal configuration as fee

--- query.sql
+++ query.sql
@@ -4,4 +4,6 @@ WHERE settings LIKE '%icon%' -- LOL, easiest way to find a lunch contract w/o jo
   AND deleted_at IS NULL
   AND status = 'accepted'
   AND (end_date IS NULL OR end_date > NOW())
-  AND partner_id NOT IN (137, 11);
+  AND partner_id NOT IN (137, 11)
+  -- 4393 has meal selection that is a fee and messes up our pax value, so we ignore it
+  AND contracts.id NOT IN (4393);
